22537c932c3a898ffe2497d841917a371fd44985
Sillas Torres <sillas.zom@gmail.com>
2022-03-06 21:07:24 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
-
+import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" olha mundo");
+	Position pos= new Position (3, 5);
+	System.out.println(pos);
 
 	}
 
