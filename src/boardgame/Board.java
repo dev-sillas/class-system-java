dd7b14562adb978e03eee662dbd7a3ad367e9c29
Sillas Torres <sillas.zom@gmail.com>
2022-03-13 21:57:07 -0300

    Starting to implement Board and Piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -10,17 +10,17 @@ public Board(int row, int column) {
 	piece= new Piece[row][column];
 	
 }
+public int getRow() {
+	return row;
+}
+public void setRow(int row) {
+	this.row = row;
+}
 public int getColumn() {
 	return column;
 }
 public void setColumn(int column) {
 	this.column = column;
-}
-public Piece[][] getPiece() {
-	return piece;
-}
-public void setPiece(Piece[][] piece) {
-	this.piece = piece;
 }
 
 }
